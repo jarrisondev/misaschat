e0b326480469d1bf2ad658e8927a12f04e9ec42b
Jarrison Cano <jarrison.cano@hotmail.com>
2021-05-26 23:18:50 -0500

    Updated userContext. changed the logic of the user

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -1,26 +1,10 @@
-import { userContext } from '../../context/userContext'
+import { UserContext } from '../../context/userContext'
 import { LayoutStyled } from './styles'
-import { useState } from 'react'
 
 export const Layout = ({ children }) => {
-	const initialUserData = {
-		email: '',
-		name: '',
-		password: '',
-	}
-	const [userData, setUserData] = useState(initialUserData)
-
-	const providerValue = {
-		user: {
-			userData,
-			setUserData,
-			initialUserData,
-		},
-	}
-
 	return (
-		<userContext.Provider value={providerValue}>
+		<UserContext>
 			<LayoutStyled>{children}</LayoutStyled>
-		</userContext.Provider>
+		</UserContext>
 	)
 }
--- context/userContext.js
+++ context/userContext.js
@@ -1,3 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const userContext = React.createContext(null)
+
+export const UserContext = ({ children }) => {
+	const initialUserData = {
+		email: '',
+		name: '',
+		password: '',
+	}
+	const [userData, setUserData] = useState(initialUserData)
+
+	const providerValue = {
+		user: {
+			userData,
+			setUserData,
+			initialUserData,
+		},
+	}
+
+	return (
+		<userContext.Provider value={providerValue}>
+			{children}
+		</userContext.Provider>
+	)
+}
