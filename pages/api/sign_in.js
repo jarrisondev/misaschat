89b5986a98b38161b00414d2f65c21f6d55ce496
Jarrison Cano <jarrison.cano@hotmail.com>
2021-05-26 23:38:39 -0500

    Issue:#2. Added compare-function

--- pages/api/sign_in.js
+++ pages/api/sign_in.js
@@ -1,20 +1,31 @@
 import userModel from '../../models/user.model'
 import { connection } from 'mongoose'
 import { connectDB } from '../../mongoDB/connect'
+import { compare } from 'bcrypt'
 
 export default async function handler(req, res) {
 	if (req.method === 'POST') {
 		connectDB()
 		const { email, password } = req.body
-		const result = await userModel.find({ email, password })
+		const result = await userModel.find({ email })
 
 		if (result.length === 1) {
-			res
-				.status(200)
-				.json({ name: result[0].name, email: result[0].email, password: null })
+			const userPassword = result[0].password
+
+			compare(password, userPassword, (err, token) => {
+				if (token) {
+					res.status(200).json({
+						name: result[0].name,
+						email: result[0].email,
+						password: '',
+					})
+				} else {
+					res.status(401).json({ message: 'Incorrect password', err })
+				}
+			})
 			await connection.close().then(() => console.log('database closed'))
 		} else {
-			res.status(406).json({ message: 'Incorrect user' })
+			res.status(401).json({ message: 'Incorrect user' })
 			await connection.close().then(() => console.log('database closed'))
 		}
 	} else {
