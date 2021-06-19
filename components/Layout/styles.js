f22d19543e9b877f0c5bf1da8aa2663fb8a39ca7
Jarrison Cano <jarrison.cano@hotmail.com>
2021-06-19 18:19:39 -0500

    Improved styles of the layout

--- components/Layout/styles.js
+++ components/Layout/styles.js
@@ -1,11 +1,12 @@
 import styled from 'styled-components'
 
 export const LayoutStyled = styled.div`
-	background: #11998e; /* fallback for old browsers */
+	background: #11998e;
 	background: linear-gradient(to bottom, #38ef7d, #11998e);
 	height: 100%;
 	max-width: 450px;
 
 	min-height: 100vh;
+	position: relative;
 	width: 100%;
 `
