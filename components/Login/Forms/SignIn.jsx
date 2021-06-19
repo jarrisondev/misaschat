ff84c9671defa914d0f2704cf4ffa7a71cb9f2d1
Jarrison Cano <jarrison.cano@hotmail.com>
2021-06-18 19:36:21 -0500

    Implemented Eslint and improve token

--- components/Login/Forms/SignIn.jsx
+++ components/Login/Forms/SignIn.jsx
@@ -1,47 +1,44 @@
-import { FormStyled } from './styles'
-import { useRouter } from 'next/router'
-import { Button } from '../../globals-components/Button'
-import { Anchor } from '../../globals-components/Anchor'
-import { signInController } from '../../../controllers/signInController'
-import { useContext } from 'react'
-import { userContext } from '../../../context/userContext'
+import {FormStyled} from './styles'
+import {useRouter} from 'next/router'
+import {Button} from '../../globals-components/Button'
+import {Anchor} from '../../globals-components/Anchor'
+import {signInController} from '../../../controllers/signInController'
 
-export const SignIn = ({ renderForm, setRenderForm }) => {
-  const router = useRouter()
-  const { user } = useContext(userContext)
+export const SignIn = ({user, renderForm, setRenderForm}) => {
+	const router = useRouter()
 
-  const sendUserData = (event) => {
-    event.preventDefault()
-    signInController(user, event, router)
-  }
-  const saveData = (event) => {
-    const input = event.target
+	const sendUserData = (event) => {
+		event.preventDefault()
+		signInController(user, event, router)
+	}
+	const saveData = (event) => {
+		const input = event.target
 
-    user.setUserData({ ...user.userData, [input.name]: input.value })
-  }
+		user.setUserData({...user.userData, [input.name]: input.value})
+	}
 
-  return (
-    <FormStyled>
-      <label>
-        <img src='/icons/Login/email.svg' alt='' />
-        <input
-          name='email'
-          type='email'
-          onChange={(event) => saveData(event)}
-          placeholder='email@example.com'
-        />
-      </label>
-      <label>
-        <img src='/icons/Login/password.svg' alt='' />
-        <input
-          name='password'
-          type='password'
-          onChange={(event) => saveData(event)}
-          placeholder='Contraseña'
-        />
-      </label>
-      <Anchor renderForm={renderForm} handler={setRenderForm} />
-      <Button renderForm={renderForm} handler={sendUserData} />
-    </FormStyled>
-  )
+	return (
+		<FormStyled>
+			<label>
+				<img src='/icons/Login/email.svg' alt='' />
+				<input
+					name='email'
+					type='email'
+					onChange={(event) => saveData(event)}
+					placeholder='email@example.com'
+				/>
+			</label>
+			<label>
+				<img src='/icons/Login/password.svg' alt='' />
+				<input
+					name='password'
+					type='password'
+					onChange={(event) => saveData(event)}
+					placeholder='Contraseña'
+				/>
+			</label>
+			<Anchor renderForm={renderForm} handler={setRenderForm} />
+			<Button renderForm={renderForm} handler={sendUserData} />
+		</FormStyled>
+	)
 }
--- controllers/signInController.js
+++ controllers/signInController.js
@@ -1,17 +1,17 @@
 export const signInController = async (user, event, router) => {
-  const res = await fetch('/api/sign_in', {
-    method: 'POST',
-    headers: new Headers([['Content-type', 'application/json']]),
-    body: JSON.stringify(user.userData)
-  })
+	const res = await fetch('/api/sign_in', {
+		method: 'POST',
+		headers: new Headers([['Content-type', 'application/json']]),
+		body: JSON.stringify(user.userData),
+	})
 
-  if (res.ok) {
-    res.json().then((token) => {
-      sessionStorage.setItem('login', JSON.stringify(token))
-      event.target.parentNode.reset()
-      router.push('/dashboard')
-    })
-  } else {
-    res.json().then((res) => alert(res.message))
-  }
+	if (res.ok) {
+		res.json().then((token) => {
+			event.target.parentNode.reset()
+			localStorage.setItem('misaschats-login', JSON.stringify(token))
+			router.push('/dashboard')
+		})
+	} else {
+		res.json().then((res) => alert(res.message))
+	}
 }
