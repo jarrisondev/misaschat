c2f0e5bdc487dde458717969126744ff30f575c4
Jarrison Cano <jarrison.cano@hotmail.com>
2021-06-18 19:37:35 -0500

    Issue:#6. Implemented Eslint and fixed

--- components/Login/Forms/SignUp.jsx
+++ components/Login/Forms/SignUp.jsx
@@ -1,91 +1,89 @@
-import { FormStyled } from './styles'
-import { Button } from '../../globals-components/Button'
-import { Anchor } from '../../globals-components/Anchor'
-import { useContext, useState } from 'react'
-import { signUpController } from '../../../controllers/signUpController'
-import { userContext } from '../../../context/userContext'
+import {FormStyled} from './styles'
+import {Button} from '../../globals-components/Button'
+import {Anchor} from '../../globals-components/Anchor'
+import {useState} from 'react'
+import {signUpController} from '../../../controllers/signUpController'
 
-export const SignUp = ({ renderForm, setRenderForm }) => {
-  const { user } = useContext(userContext)
+export const SignUp = ({user, renderForm, setRenderForm}) => {
+	const initialInputsCheck = {
+		name: false,
+		email: false,
+		password: false,
+	}
+	const [inputsCheck, setInputsCheck] = useState(initialInputsCheck)
+	const [requestInProgress, setRequestInProgress] = useState(true)
 
-  const initialInputsCheck = {
-    name: false,
-    email: false,
-    password: false
-  }
-  const [inputsCheck, setInputsCheck] = useState(initialInputsCheck)
-  // const [handlerButton, setHandlerButton] = useState(true)
+	const saveUserData = (event) => {
+		event.preventDefault()
+		if (inputsCheck.name && inputsCheck.email && inputsCheck.password) {
+			// this is a controller
+			requestInProgress &&
+				signUpController(event, setRenderForm, user, setRequestInProgress)
+		} else {
+			alert('datos incorrectos')
+		}
+	}
 
-  const saveUserData = (event) => {
-    event.preventDefault()
-    if (inputsCheck.name && inputsCheck.email && inputsCheck.password) {
-      // this is a controller
-      signUpController(event, renderForm, user)
-    } else {
-      alert('datos incorrectos')
-    }
-  }
+	const checkInputData = (e) => {
+		const input = e.target
+		user.setUserData({...user.userData, [input.name]: input.value})
 
-  const checkInputData = (e) => {
-    const input = e.target
-    user.setUserData({ ...user.userData, [input.name]: input.value })
+		switch (input.name) {
+			case 'name':
+				;/^[a-zA-ZÀ-ÿ\s]{1,50}$/.test(input.value)
+					? setInputsCheck({...inputsCheck, name: true})
+					: setInputsCheck({...inputsCheck, name: false})
+				break
+			case 'email':
+				;/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(input.value)
+					? setInputsCheck({...inputsCheck, email: true})
+					: setInputsCheck({...inputsCheck, email: false})
+				break
+			case 'password':
+				;/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/.test(
+					input.value
+				)
+					? setInputsCheck({...inputsCheck, password: true})
+					: setInputsCheck({...inputsCheck, password: false})
+				break
+		}
+	}
 
-    switch (input.name) {
-      case 'name':
-        ;/^[a-zA-ZÀ-ÿ\s]{1,50}$/.test(input.value)
-          ? setInputsCheck({ ...inputsCheck, name: true })
-          : setInputsCheck({ ...inputsCheck, name: false })
-        break
-      case 'email':
-        ;/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(input.value)
-          ? setInputsCheck({ ...inputsCheck, email: true })
-          : setInputsCheck({ ...inputsCheck, email: false })
-        break
-      case 'password':
-        ;/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/.test(
-          input.value
-        )
-          ? setInputsCheck({ ...inputsCheck, password: true })
-          : setInputsCheck({ ...inputsCheck, password: false })
-        break
-    }
-  }
-
-  return (
-    <FormStyled>
-      <label>
-        <img src='/icons/Login/user.svg' alt='user icon' />
-        <input
-          name='name'
-          onChange={(event) => checkInputData(event)}
-          type='text'
-          placeholder='Nombre'
-          required
-        />
-      </label>
-      <label>
-        <img src='/icons/Login/email.svg' alt='email icon' />
-        <input
-          name='email'
-          onChange={(event) => checkInputData(event)}
-          type='email'
-          placeholder='email@example.com'
-          required
-        />
-      </label>
-      <label>
-        <img src='/icons/Login/password.svg' alt='password' />
-        <input
-          name='password'
-          onChange={(event) => checkInputData(event)}
-          type='password'
-          placeholder='Contraseña'
-          required
-          maxLength='15'
-        />
-      </label>
-      <Anchor renderForm={renderForm} handler={setRenderForm} />
-      <Button renderForm={renderForm} handler={saveUserData} />
-    </FormStyled>
-  )
+	return (
+		<FormStyled>
+			<label>
+				<img src='/icons/Login/user.svg' alt='user icon' />
+				<input
+					name='name'
+					onChange={(event) => checkInputData(event)}
+					type='text'
+					placeholder='Nombre'
+					required
+				/>
+			</label>
+			<label>
+				<img src='/icons/Login/email.svg' alt='email icon' />
+				<input
+					name='email'
+					onChange={(event) => checkInputData(event)}
+					type='email'
+					placeholder='email@example.com'
+					required
+				/>
+			</label>
+			<label>
+				<img src='/icons/Login/password.svg' alt='password' />
+				<input
+					name='password'
+					onChange={(event) => checkInputData(event)}
+					type='password'
+					placeholder='Contraseña'
+					required
+					maxLength='15'
+				/>
+			</label>
+			<Anchor renderForm={renderForm} handler={setRenderForm} />
+			<Button renderForm={renderForm} handler={saveUserData} />
+		</FormStyled>
+	)
 }
--- controllers/signUpController.js
+++ controllers/signUpController.js
@@ -1,15 +1,23 @@
-export const signUpController = async (event, setRenderForm, user) => {
-  const res = await fetch('/api/sign_up', {
-    method: 'POST',
-    headers: new Headers([['Content-type', 'application/json']]),
-    body: JSON.stringify(user.userData)
-  })
+export const signUpController = async (
+	event,
+	setRenderForm,
+	user,
+	setRequestInProgress
+) => {
+	setRequestInProgress(false)
+	const res = await fetch('/api/sign_up', {
+		method: 'POST',
+		headers: new Headers([['Content-type', 'application/json']]),
+		body: JSON.stringify(user.userData),
+	})
 
-  if (res.ok) {
-    // event.target.parentNode.reset()
-    user.setUserData(user.initialUserData)
-    setRenderForm((r) => !r)
-  } else {
-    res.json().then((res) => alert(res.message))
-  }
+	if (res.ok) {
+		event.target.parentNode.reset()
+		user.setUserData(user.initialUserData)
+		setRequestInProgress(true)
+		setRenderForm((r) => !r)
+	} else {
+		setRequestInProgress(true)
+		res.json().then((res) => alert(res.message))
+	}
 }
