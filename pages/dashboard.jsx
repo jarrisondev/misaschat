d382a1032157e2ea294adff5e993840a0bf836d4
Jarrison Cano <jarrison.cano@hotmail.com>
2021-06-28 23:03:47 -0500

    :fire: refactor: improve structure dashboard page

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -1,10 +1,51 @@
-import { Dashboard } from 'components/Dashboard/Dashboard'
+import { useRouter } from 'next/router'
+import { MainStyled } from 'styles/dashboard'
 import { Layout } from 'components/Layout/Layout'
+import { UtilsContext } from 'context/utilsContext'
+import { useContext, useEffect, useState } from 'react'
+import { getContactsController } from 'controllers/dashboardController'
+
 export default function dashboard () {
+  const { JWT_TOKEN_NAME } = useContext(UtilsContext)
+  const router = useRouter()
+  const [contacts, setContacts] = useState([])
+
+  const SignOut = () => {
+    window.localStorage.removeItem(JWT_TOKEN_NAME)
+    router.push('/')
+  }
+
+  useEffect(async () => {
+    const token = JSON.parse(window.localStorage.getItem(JWT_TOKEN_NAME))
+    const contacts = await getContactsController(token, router, JWT_TOKEN_NAME)
+    setContacts(contacts)
+  }, [])
+
   return (
     <>
       <Layout>
-        <Dashboard />
+        <MainStyled>
+          <h1>Dashboard</h1>
+          {contacts &&
+            contacts.map((contact, i) => {
+              return (
+                <div key={i}>
+                  {contact.name} - {contact.email}
+                </div>
+              )
+            })}
+          <br />
+          <br />
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse
+            cupiditate qui, beatae reiciendis libero sit doloremque modi
+            veritatis delectus repudiandae!
+          </p>
+          <h2> Cerrar sesión</h2>
+          <button type='button' onClick={SignOut}>
+            Cerrar sesión
+          </button>
+        </MainStyled>
       </Layout>
     </>
   )
