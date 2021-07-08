1d74a9fbaf1d46dc49bcbe584e680d83fd1cf720
Jarrison Cano <jarrison.cano@hotmail.com>
2021-07-07 23:25:49 -0500

    :art: refactor: add a image option to the button-component

--- components/globals-components/Button/Button.jsx
+++ components/globals-components/Button/Button.jsx
@@ -1,9 +1,10 @@
 import { ButtonStyled } from './styles'
 
-export const Button = ({ text, handler = null }) => {
+export const Button = ({ text, imgURL, handler = null }) => {
   return (
     <ButtonStyled onClick={(event) => handler ? handler(event) : ' '} type='submit'>
-      {text}
+      {text && text}
+      {imgURL && <img src={imgURL} alt='generate image' />}
     </ButtonStyled>
   )
 }
