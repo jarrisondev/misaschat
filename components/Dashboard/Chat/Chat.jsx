ee19b0f17e583acf4340399679cbf6d67e2ddbc0
Jarrison Cano <jarrison.cano@hotmail.com>
2021-07-22 19:40:28 -0500

    :construction: introduce socket.io

--- components/Dashboard/Chat/Chat.jsx
+++ components/Dashboard/Chat/Chat.jsx
@@ -3,16 +3,34 @@ import { Input } from 'components/globals-components/Input/Input'
 import { Layout } from 'components/Layout/Layout'
 import { useForm } from 'react-hook-form'
 import { ChatStyled } from './styles'
+import { useEffect, useState } from 'react'
+import { io } from 'socket.io-client'
 
 export const Chat = ({ chat, setChat }) => {
+  const [socket, setSocket] = useState()
+  const [messages, setMessages] = useState(chat.messages)
+  const { register, handleSubmit, setValue } = useForm()
+
   const goBack = () => {
     setChat(null)
   }
-  const {
-    register,
-    formState: { errors }
-  } = useForm()
-  console.log(chat)
+
+  const onSubmit = (message) => {
+    setValue('message')
+    setMessages((m) => [...m, message.message])
+    socket.emit(chat._id, message)
+  }
+
+  useEffect(() => {
+    setSocket(io(process.env.SOCKET_IO_URL))
+  }, [])
+
+  useEffect(() => {
+    socket?.on(chat._id, (values) => {
+      setMessages((m) => [...m, values.message])
+    })
+  }, [socket])
+
   return (
     <Layout>
       <ChatStyled>
@@ -20,30 +38,21 @@ export const Chat = ({ chat, setChat }) => {
           <Button handler={goBack} imgURL='/icons/chat/back.svg' />
           <h2>{chat.contactName}</h2>
         </header>
-        {/* <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores,
-          delectus.
-        </p> */}
         <div className='chat-content'>
-          {chat.messages.map((message) => {
-            if (message.me) {
-              return <p /> // render the p in rigth
-            } else {
-              return <p /> // render the p in left
-            }
-          })}
+          {messages.map((message, i) => (
+            <p key={i}>{message}</p>
+          ))}
         </div>
-        <div className='input-content'>
+        <form onSubmit={handleSubmit(onSubmit)} className='input-content'>
           <Input
             register={register}
-            errors={errors}
             placeholder='Enviar un Mensaje'
             name='message'
             maxLength={500}
-            required={false}
+            warnnings={false}
           />
           <Button imgURL='/icons/chat/send-icon.svg' />
-        </div>
+        </form>
       </ChatStyled>
     </Layout>
   )
