c5689ea7784795bc96b9dca1607a752dcd6c4d87
Jarrison Cano <jarrison.cano@hotmail.com>
2021-08-14 17:59:35 -0500

    :recycle: move code to custom hooks

--- hooks/useChats.js
+++ hooks/useChats.js
@@ -1,20 +1,41 @@
-import { ModalContext } from 'context/modalContext'
-import { getChatsController } from 'controllers/dashboardController'
+import { useContext } from 'react'
 import { useRouter } from 'next/router'
-import { useContext, useEffect, useState } from 'react'
+import { ModalContext } from 'context/modalContext'
+import { DashboardContext } from 'context/dashboardContext'
+import { getChatsController } from 'controllers/dashboardController'
 
 export const useChats = () => {
   const router = useRouter()
   const { setModal } = useContext(ModalContext)
+  const { store, setStore } = useContext(DashboardContext)
 
-  const [chats, setChats] = useState(null)
+  const chats = store.chats
+  const activeChat = store.activeChat
 
-  useEffect(async () => {
+  const renderMessage = (message) => {
+    const newChats = chats?.chats.map((chat) => {
+      chat._id === activeChat._id && chat.messages.push(message)
+      return chat
+    })
+
+    setStore({
+      ...store,
+      chats: newChats
+    })
+  }
+
+  const getChats = async () => {
     const res = await getChatsController(router, setModal)
-    setChats(res)
-  }, [])
+    setStore({
+      ...store,
+      chats: res
+    })
+  }
 
   return {
-    chats
+    getChats,
+    renderMessage,
+    chats,
+    activeChat
   }
 }
