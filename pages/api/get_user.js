62b4c81c74b0b2fcbcfc23d50f1913ab8c574064
Jarrison Cano <jarrison.cano@hotmail.com>
2021-11-07 14:40:42 -0500

    add the API get_user

---
+++ pages/api/get_user.js
@@ -0,0 +1,27 @@
+import UserModel from 'mongoDB/models/user.model'
+const { verify } = require('jsonwebtoken')
+const { connectDB } = require('mongoDB/connect')
+
+function getUser (req, res) {
+  if (req.method === 'GET') {
+    const { authorization } = req.headers
+    const tokenUser = authorization.substring(7)
+
+    verify(tokenUser, process.env.JWT_SIGN, (err, decoded) => {
+      if (decoded) {
+        connectDB() //
+          .then(() => UserModel.findById(decoded.id))
+          .then((user) => {
+            res.status(200).json({ name: user.name, id: user._id })
+          })
+          .catch(() => {
+            res.status(401).json({ message: 'error in the get user' })
+          })
+      } else {
+        res.status(401).json({ message: 'Unauthorized', err })
+      }
+    })
+  }
+}
+
+export default getUser
