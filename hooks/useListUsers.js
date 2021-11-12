9fc5bba09469a2bc4c3c12d83bad8c06ffe12e3e
Jarrison Cano <jarrison.cano@hotmail.com>
2021-11-11 23:00:15 -0500

    :sparkles: feat: new hook for handler the UsersList-section

---
+++ hooks/useListUsers.js
@@ -0,0 +1,62 @@
+import { useContext } from 'react'
+import { useChats } from './useChats'
+import { ModalContext } from 'context/modalContext'
+import { DashboardContext } from 'context/dashboardContext'
+import {
+  createChatController,
+  getUsersController
+} from 'controllers/dashboardController'
+
+export const useListUsers = () => {
+  const { setModal } = useContext(ModalContext)
+  const { setActiveChat, getChats } = useChats()
+  const { store, setStore, router, socket } = useContext(DashboardContext)
+
+  const chats = store.chats
+  const renderUsersList = store.renderUsersList
+  const usersList = store.usersList
+
+  const createChat = async (user) => {
+    const [chat] = chats.filter((chat) => chat.users.includes(user.id))
+
+    if (chat) setActiveChat(chat)
+    else {
+      const NewChat = await createChatController(router, setModal, user)
+      const chats = await getChats()
+
+      // this is for update the chatList of the other chat-member
+      const [id] = NewChat.users.filter((id) => id !== chats.userId)
+      socket.emit(id)
+    }
+
+    setStore({
+      ...store,
+      renderUsersList: false
+    })
+  }
+
+  const getUsers = async () => {
+    const users = await getUsersController(router, setModal)
+
+    setStore((store) => ({
+      ...store,
+      usersList: users
+    }))
+  }
+
+  const handlerUsersList = (newState) => {
+    setStore((state) => ({
+      ...state,
+      renderUsersList: newState
+    }))
+  }
+
+  return {
+    socket,
+    usersList,
+    renderUsersList,
+    getUsers,
+    createChat,
+    handlerUsersList
+  }
+}
