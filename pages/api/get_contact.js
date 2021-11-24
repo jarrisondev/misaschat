2b963604d60582e2dd0a1a1a7da43515fd647e6c
Jarrison Cano <jarrison.cano@hotmail.com>
2021-11-23 21:48:18 -0500

    :zap: refactor: improve structure of code for the get_contact API

--- pages/api/get_contact.js
+++ pages/api/get_contact.js
@@ -4,15 +4,19 @@ import UserModel from 'mongoDB/models/user.model'
 
 function getContact (req, res) {
   if (req.method === 'GET') {
-    const contactId = req.query
+    const { id } = req.query
     const { authorization } = req.headers
     const tokenUser = authorization.substring(7)
 
     verify(tokenUser, process.env.JWT_SIGN, async (err, decoded) => {
       if (decoded) {
         await connectDB()
-        const contact = await UserModel.findById(contactId)
-        if (contact) { res.status(200).json({ name: contact.name, id: contact.id }) } else res.status(404).json({ message: 'error in the get user' })
+        const contact = await UserModel.findById(id)
+        if (contact) {
+          res.status(200).json({ name: contact.name, id: contact.id })
+        } else {
+          res.status(404).json({ message: 'error in the get user' })
+        }
       } else {
         res.status(401).json({ message: 'Unauthorized', err })
       }
