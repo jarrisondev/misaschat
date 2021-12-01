09af9e0d6a781eb14624ed5799e2d6d48d45c01d
Jarrison Cano <jarrison.cano@hotmail.com>
2021-11-30 21:21:28 -0500

    :zap: implement the news hooks in the Chat-component (Refactoring pending :poop:)

--- components/Dashboard/Chat/Chat.jsx
+++ components/Dashboard/Chat/Chat.jsx
@@ -1,37 +1,40 @@
-import { useContext } from 'react'
 import { ChatStyled } from './styles'
+import { useUser } from 'hooks/useUser'
 import { useForm } from 'react-hook-form'
+import { useChats } from 'hooks/useChats'
+import { useEffect, useState } from 'react'
 import { Layout } from 'components/Layout/Layout'
-import { DashboardContext } from 'context/dashboardContext'
 import { Input } from 'components/globals-components/Input/Input'
 import { Button } from 'components/globals-components/Button/Button'
 
 export const Chat = () => {
-  const { activeChat, setActiveChat, socket, userName, renderNewMessage } =
-    useContext(DashboardContext)
-
+  const { user, socket, getContact } = useUser()
   const { register, handleSubmit, setValue } = useForm()
-
+  const [contactName, setContactName] = useState('Cargando...')
+  const { activeChat, renderMessage, setActiveChat } = useChats()
+  const [contactId] = activeChat?.users?.filter((id) => id !== user.id) || []
   const messages = activeChat?.messages
-  const name = activeChat?.names.find((name) => name !== userName)
-
-  const goBack = () => {
-    setActiveChat(null)
-  }
 
   const onSubmit = (message) => {
     setValue('textContent')
-    renderNewMessage(activeChat, message)
+    renderMessage(message)
     socket.emit(activeChat?._id, message)
   }
+
+  useEffect(() => {
+    getContact(contactId, setContactName)
+  }, [activeChat])
 
   return (
     !activeChat || (
       <Layout>
         <ChatStyled>
           <header className='header-content'>
-            <Button handler={goBack} imgURL='/icons/chat/back.svg' />
-            <h2>{name}</h2>
+            <Button
+              handler={() => setActiveChat(false)}
+              imgURL='/icons/chat/back.svg'
+            />
+            <h2>{contactName}</h2>
           </header>
           <div className='chat-content'>
             {messages.map((message, i) => (
